aa9ce79bdbfff36cc2eb1b661f29b474952c94ca
ericgondran <ericgondran@furg.br>
2023-11-15 19:31:55 -0300

    Add course page and list course

---
+++ courses/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from courses import views
+
+app_name = "courses"
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<slug>/', views.details, name='details'),
+]
