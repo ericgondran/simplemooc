b82a60abc37643b5d4392723a3296f9344c1922d
ericgondran <ericgondran@furg.br>
2023-11-16 19:08:32 -0300

    Add form and mail to contact

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -125,5 +125,17 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# E-mails
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # email padrão
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # email teste do console
+DEFAULT_FROM_EMAIL = 'Nome <email@gmail.com>'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'senha'
+EMAIL_PORT = 587
+
+CONTACT_EMAIL = 'contato@simplemooc.com'
