ef71f3c2c31c86f0a716b186eadf872ce0fe9c50
ericgondran <ericgondran@furg.br>
2023-11-22 16:39:25 -0300

    Add view login, mail unique, register form, user panel, account and password edit. Static folder correction.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,10 @@ from django.contrib.auth import views as auth_views
 
 app_name = "accounts"
 urlpatterns = [
+    path("", views.dashboard, name='dashboard'),
     path("Entrar/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
     path("Sair/", auth_views.LogoutView.as_view(next_page="core:home"), name='logout'),
     path("Cadastre-se/", views.register, name='register'),
+    path("Editar/", views.edit, name='edit'),
+    path("Editar-senha/", views.edit_password, name='edit_password'),
 ]
