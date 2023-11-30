c29238fe56f3ed5d464bf00a73d1199e7195d144
ericgondran <ericgondran@furg.br>
2023-11-30 16:08:24 -0300

    Add forum listing by tag.

--- forum/urls.py
+++ forum/urls.py
@@ -4,4 +4,5 @@ from forum import views
 app_name = "forum"
 urlpatterns = [
     path('', views.ForumView.as_view(), name='index'),
+    path('tag/<tag>', views.ForumView.as_view(), name='index_tagged'),
 ]
