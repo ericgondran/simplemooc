761841158a5387f256a9fb7c1a84871ec1b55be7
ericgondran <ericgondran@furg.br>
2023-12-05 19:11:40 -0300

    Add view thread, answer and answer correct.

--- forum/urls.py
+++ forum/urls.py
@@ -5,4 +5,7 @@ app_name = "forum"
 urlpatterns = [
     path('', views.ForumView.as_view(), name='index'),
     path('tag/<tag>', views.ForumView.as_view(), name='index_tagged'),
+    path('<slug>', views.ThreadView.as_view(), name='thread'),
+    path('respostas/<pk>/correta/', views.ReplyCorrectView.as_view(), name='reply_correct'),
+    path('respostas/<pk>/incorreta/', views.ReplyCorrectView.as_view(correct=False), name='reply_incorrect'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
-
-from .models import Thread
+from django.views.generic import ListView , View, DetailView
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponse
+from .models import Thread, Reply
+from .forms import ReplyForm
+import json
 
 class ForumView(ListView):
 
@@ -23,3 +27,60 @@ class ForumView(ListView):
         context = super(ForumView, self).get_context_data(**kwargs)
         context['tags'] = Thread.tags.all()
         return context
+    
+class ThreadView(DetailView):
+
+    model = Thread 
+    template_name = 'forum/thread.html'
+
+    def get(self, request, *args, **kwargs):
+        response = super(ThreadView, self).get(request, *args, **kwargs)
+        if not self.request.user.is_authenticated or \
+            (self.object.author != self.request.user): # type: ignore
+            self.object.views = self.object.views + 1 # type: ignore
+            self.object.save()
+        return response
+
+    def get_context_data(self, **kwargs):
+        context = super(ThreadView, self).get_context_data(**kwargs)
+        context['tags'] = Thread.tags.all()
+        context['form'] = ReplyForm(self.request.POST or None)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request, 
+                'Para responder ao tópico é necessário estar logado'
+            )
+            return redirect(self.request.path)
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        form = context['form']
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.thread = self.object
+            reply.author = self.request.user
+            reply.save()
+            messages.success(
+                self.request, 'A sua responsta foi enviada com sucesso'
+            )
+            context['form'] = ReplyForm()
+        return self.render_to_response(context)
+
+
+class ReplyCorrectView(View):
+
+    correct = True
+
+    def get(self, request, pk):
+        reply = get_object_or_404(Reply, pk=pk, thread__author=request.user)
+        reply.correct = self.correct
+        reply.save()
+        message = 'Resposta atualizada com sucesso'
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest': # é uma requisição ajax?
+            data = {'success': True, 'message': message}
+            return HttpResponse(json.dumps(data), content_type='application/json')
+        else:
+            messages.success(request, message)
+            return redirect(reply.thread.get_absolute_url())
